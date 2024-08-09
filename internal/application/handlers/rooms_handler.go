a37d55ef18ca0ec2662c915d848ec2d65aab7d67
Ray Kochenderfer <rayjkochenderfer@gmail.com>
2024-08-08 20:59:19 -0400

    Setup room grpc

--- internal/application/handlers/rooms_handler.go
+++ internal/application/handlers/rooms_handler.go
@@ -3,23 +3,36 @@ package handlers
 import (
 	"context"
 	pb "home-manager/server/homemanager/gen"
+	"home-manager/server/internal/application/mappers"
+	"home-manager/server/internal/core/entities"
+	"home-manager/server/internal/infrastructure/services"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type RoomsHandler struct {
+	roomsService services.RoomService
 	pb.UnimplementedRoomServiceServer
 }
 
-func NewRoomsServer(grpcServer *grpc.Server) {
-	roomsHandler := &RoomsHandler{}
-	pb.RegisterRoomServiceServer(grpcServer, roomsHandler)
+func NewRoomsServer(grpcServer *grpc.Server, roomService *services.RoomService) {
+	roomsGrpc := &RoomsHandler{roomsService: *roomService}
+	
+	pb.RegisterRoomServiceServer(grpcServer, roomsGrpc)
 }
 
-func (*RoomsHandler) Create(ctx context.Context, req *pb.CreateRoomRequest) (*pb.CreateRoomResponse, error) {
-	test := pb.CreateRoomResponse{
-		Id:   1,
-		Name: req.GetName(),
+func (rh *RoomsHandler) Create(ctx context.Context, req *pb.CreateRoomRequest) (*pb.CreateRoomResponse, error) {
+	newRoom, err := entities.NewRoom(0, req.Name)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
-	return &test, nil
+
+	room, err := rh.roomsService.Create(newRoom)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return mappers.ToCreateRoomResponse(room), nil
 }
--- internal/application/mappers/rooms_response_mapper.go
+++ internal/application/mappers/rooms_response_mapper.go
@@ -3,6 +3,7 @@ package mappers
 import (
 	"home-manager/server/internal/application/models"
 	"home-manager/server/internal/core/entities"
+	pb "home-manager/server/homemanager/gen"
 )
 
 func ToRoomsListResponse(rooms []*entities.Room) []*models.RoomResponse {
@@ -14,4 +15,35 @@ func ToRoomsListResponse(rooms []*entities.Room) []*models.RoomResponse {
 	}
 
 	return responseList
+}
+
+func ToCreateRoomResponse(room *entities.Room) *pb.CreateRoomResponse {
+	return &pb.CreateRoomResponse{
+		Room: toRoom(room),
+	}
+}
+
+func ToGetRoomResponse(room *entities.Room) *pb.GetRoomResponse {
+	return &pb.GetRoomResponse{
+		Room: toRoom(room),
+	}
+}
+
+func ToGetAllRoomsResponse(rooms *[]entities.Room) *pb.GetAllRoomsResponse {
+	var roomsList []*pb.Room
+	for _, r := range *rooms {
+		er := toRoom(&r)
+		roomsList = append(roomsList, er)
+	}
+
+	return &pb.GetAllRoomsResponse{
+		Rooms: roomsList,
+	}
+}
+
+func toRoom(room *entities.Room) *pb.Room {
+	return &pb.Room{
+		Id: room.GetId(),
+		Name: room.GetName(),
+	}
 }
