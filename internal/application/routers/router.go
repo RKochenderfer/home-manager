3bca0b53fb19990b947950752c82d5c9de1bb458
Ray Kochenderfer <rayjkochenderfer@gmail.com>
2024-08-09 14:07:27 -0400

    Revert "Remove http endpoints"
    This reverts commit 126ae060a1e3ff17e6775d08aaf2ef44a56a87c5.

---
+++ internal/application/routers/router.go
@@ -0,0 +1,50 @@
+package routers
+
+import (
+	controllers "home-manager/server/internal/application/controllers"
+	"home-manager/server/internal/infrastructure/db"
+	"home-manager/server/internal/infrastructure/repositories"
+	"home-manager/server/internal/infrastructure/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(db db.Database) *gin.Engine {
+	router := gin.Default()
+
+	// Public routes
+	setupUsersRoutes(router, &db)
+	setupRoomsRoutes(router, &db)
+
+	// Protected Routes
+
+	return router
+}
+
+func setupUsersRoutes(router *gin.Engine, db *db.Database) {
+	repo := repositories.NewSqliteUserRepo(db)
+	us, _ := services.NewUsersService(repo)
+
+	uc := controllers.NewUsersController(us)
+
+	chores := router.Group("api/users")
+	{
+		chores.GET("/", uc.GetAll)
+		chores.GET("/:id", uc.GetById)
+		chores.POST("/", uc.Create)
+	}
+}
+
+func setupRoomsRoutes(router *gin.Engine, db *db.Database) {
+	repo := repositories.NewSqliteRoomRepo(db)
+	rs, _ := services.NewRoomService(repo)
+
+	rc := controllers.NewRoomsController(rs)
+
+	rooms := router.Group("api/rooms")
+	{
+		rooms.GET("/", rc.GetAll)
+		rooms.GET("/:id", rc.GetById)
+		rooms.POST("/", rc.Create)
+	}
+}
